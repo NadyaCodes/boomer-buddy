f7057e98eba38fec1c3099cc88fc3bd4375c8cc5
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-02 18:12:43 -0400

    add random fact to page

--- pages/fact.js
+++ pages/fact.js
@@ -1,18 +1,40 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
+export async function findFact() {
+  const data = await fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+  const jsonData = await data.json()
+
+  const fact = jsonData.text
+
+  return fact;
+}
 
 
-export default function Fact() {
+export async function getStaticProps() {
+
+  const factData = await findFact()
+  return {
+    props: {
+      factData,
+    }
+  }
+
+}
+
+
+
+export default function Fact( {factData} ) {
   return(
     <>
       <Head>
         <title>Random Fact</title>
       </Head>
       <h1>Random Fact</h1>
-      <Link href="/">
+      <p>{factData}</p>
+      <button><Link href="/">
         <a>Back to home</a>
-      </Link>
+      </Link></button>
     </>
   )
 }
