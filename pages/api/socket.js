26f7ac2578c6ce228f0c54eb73893867b05c826d
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-03 19:33:47 -0400

    install socket.io and set up basic functionality

---
+++ pages/api/socket.js
@@ -0,0 +1,26 @@
+// export default function handler(req, res) {}
+
+
+import {Server} from 'Socket.IO'
+import { io } from 'socket.io-client'
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('input-change', msg => {
+        socket.broadcast.emit('update-input', msg)
+      })
+    })
+  }
+
+
+  res.end()
+}
+
+export default SocketHandler
--- pages/groups.js
+++ pages/groups.js
@@ -1,17 +1,48 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+let socket
 
 export default function Groups() {
   const router = useRouter()
   const { user } = router.query
+  const [input, setInput] = useState('')
+
+  useEffect(() => {socketInitializer()}, [])
+
+  const socketInitializer = async () => {
+    await fetch('/api/socket')
+    socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+
+    socket.on('update-input', msg => {
+      setInput(msg)
+    })
+  }
+
   
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value)
+    socket.emit('input-change', e.target.value)
+  }
+
   return (
     <>
       <Head>
         <title>Groups</title>
       </Head>
       <h1>Groups</h1>
+      <input placeholder='Type Something'
+      value={input}
+      onChange={onChangeHandler}
+      />
+
       <button>
       <Link href={{
         pathname: "/home",
